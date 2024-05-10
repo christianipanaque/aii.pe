0e2307638ff97a86d3c7488e0fae323e6b3da45d
Christian Ipanaque <christian.ipanaque@engineer.com>
2024-05-10 12:52:11 -0500

    update images

--- assets/button.js
+++ assets/button.js
@@ -1,3 +1,3 @@
 function whatsApp() {
-  window.location.href = `https://wa.me/51913607355?text=Quiero%20comprar%20el%20libro%20"Experto%20en%20ChatGPT"%20ya!`;
+  window.location.href = `https://wa.me/51913607355?text=Quiero%20comprar%20mi%20libro%20"Experto%20en%20ChatGPT"%20ya!`;
 }
